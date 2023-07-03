089c53de632ef1e79c0e129ec139d5f672ad134e
Mauricio Salatino <salaboy@salaboy.local>
2023-07-03 09:11:58 +0200

    updating tests and spec
    Signed-off-by: salaboy <msalatino@vmware.com>

---
+++ statestore-api/http-1/README.md
@@ -0,0 +1,6 @@
+# HTTP-1 OpenAPI Spec
+
+This files are handcrafted based on the APIs located here: https://github.com/dapr/dapr/blob/master/pkg/http/api.go#L255 and in the discussion started here: https://github.com/dapr/dapr/issues/2817
+
+
+
--- tck/statestore_tck_test.go
+++ tck/statestore_tck_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"context"
+	"io"
 	"net/http"
 	"testing"
 
@@ -16,7 +17,7 @@ import (
 var statestoreName = "statestore"
 var daprURLHTTP1 = "http://localhost:3500"
 
-func Test_storeStateAPIs(t *testing.T) {
+func Test_storeStateAPIsBasic(t *testing.T) {
 
 	ctx := context.Background()
 	client, err := clients.NewClient(daprURLHTTP1)
@@ -26,26 +27,44 @@ func Test_storeStateAPIs(t *testing.T) {
 
 	body := `[{ "key": "state_1", "value": "init"}]`
 
-	resp, err := client.StoreStateWithBody(ctx, statestoreName, "application/json", bytes.NewReader([]byte(body)))
+	resp, err := client.PostStoreStateWithBody(ctx, statestoreName, "application/json", bytes.NewReader([]byte(body)))
 
 	if err != nil {
-		panic(err)
+		assert.Fail(t, "", err)
 	}
 
 	assert.Equal(t, http.StatusNoContent, resp.StatusCode)
 
 	resp, err = client.GetState(ctx, statestoreName, "state_1", &clients.GetStateParams{})
 	if err != nil {
-		panic(err)
+		assert.Fail(t, "", err)
 	}
 
 	var payload string
 	err = json.NewDecoder(resp.Body).Decode(&payload)
 	if err != nil {
-		panic(err)
+		assert.Fail(t, "", err)
 	}
 
 	assert.Equal(t, http.StatusOK, resp.StatusCode)
 	assert.Equal(t, "init", payload)
+
+	resp, err = client.DeleteState(ctx, statestoreName, "state_1")
+	if err != nil {
+		assert.Fail(t, "", err)
+	}
+	assert.Equal(t, 204, resp.StatusCode)
+
+	resp, err = client.GetState(ctx, statestoreName, "state_1", &clients.GetStateParams{})
+	if err != nil {
+		assert.Fail(t, "", err)
+	}
+	var emptyPayload string
+	err = json.NewDecoder(resp.Body).Decode(emptyPayload)
+
+	assert.Equal(t, io.EOF, err)
+}
+
+func Test_bulkStoreStateAPIs(t *testing.T) {
 
 }
